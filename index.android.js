00be17303ccc3cce18a90d54acfa0d7440190eda
Andrew Wilson <andero_wil@hotmail.com>
2017-07-14 14:23:41 +0200

    Add welcoming message in android

--- index.android.js
+++ index.android.js
@@ -26,6 +26,9 @@ export default class react_native_demo extends Component {
           Double tap R on your keyboard to reload,{'\n'}
           Shake or press menu button for dev menu
         </Text>
+        <Text style={styles.instructions}>
+	Welcome to MeetBuzz!
+        </Text>
       </View>
     );
   }
